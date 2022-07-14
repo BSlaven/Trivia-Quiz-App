52894356bcf0d468461292f886977ea5bdc5c344
SlavenB <slaven.bunijevac@gmail.com>
2022-07-14 20:05:11 +0200

    add slices to store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import playerSlice from '../slices/playerSlice';
+import quizSlice from '../slices/quizSlice';
+
 export const store = configureStore({
   reducer: {
-    
+    // modalSlice,
+    playerSlice,
+    quizSlice
   },
 });
