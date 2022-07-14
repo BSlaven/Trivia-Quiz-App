c861ca0e9e13b1f8833d3a1ebecbea964a7a6ef0
SlavenB <slaven.bunijevac@gmail.com>
2022-07-14 20:02:49 +0200

    create playerSlice and quizSlice

---
+++ src/redux/carts/playerSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+
+}
+
+const playerSlice = createSlice({
+  name: 'player',
+  initialState,
+  reducers: {
+    
+  }
+})
+
+export default playerSlice.reducer;
