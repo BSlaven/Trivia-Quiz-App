76c4affeea6239842e5a81722eb963848ec7efb0
SlavenB <slaven.bunijevac@gmail.com>
2022-07-16 22:16:22 +0200

    add styling for question number container

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='home'>
+      {/* <h1 className="home-page-title">Select a number of choices and start the game</h1> */}
       <div className="question-number-container">
-        <span className="question-number ten">10</span>
-        <span className="question-number ten">15</span>
-        <span className="question-number ten">20</span>
+        <span className="question-number ten selected">10</span>
+        <span className="question-number fifteen">15</span>
+        <span className="question-number twenty">20</span>
       </div>
       <button className="start-game-btn">Start Game</button>
     </div>
