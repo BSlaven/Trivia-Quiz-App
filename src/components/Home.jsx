e0dc3481c9665478708a90f21fc74800a949c6b5
SlavenB <slaven.bunijevac@gmail.com>
2022-07-16 22:22:57 +0200

    remove extra css lines and classes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className='home'>
       {/* <h1 className="home-page-title">Select a number of choices and start the game</h1> */}
       <div className="question-number-container">
-        <span className="question-number ten selected">10</span>
+        <span className="question-number ten">10</span>
         <span className="question-number fifteen">15</span>
         <span className="question-number twenty">20</span>
       </div>
