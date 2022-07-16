573a81772b6ada380526efc407089570434fd968
SlavenB <slaven.bunijevac@gmail.com>
2022-07-16 21:47:25 +0200

    introduce Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Ovo je Home stranica</div>
+  )
+}
+
+export default Home
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import playerSlice from '../slices/playerSlice';
 import quizSlice from '../slices/quizSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     // modalSlice,
     player: playerSlice,
     quiz: quizSlice
   },
 });
+
+export default store
