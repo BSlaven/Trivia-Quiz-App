dac59f3b9326f18bdeb9d2ea6bf17d05373f19aa
SlavenB <slaven.bunijevac@gmail.com>
2022-07-20 22:59:39 +0200

    set number of questions as a dynamic property in quiz slice

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import { useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { numberOfQuestions } = useSelector(store => store.quiz);
+
+  const startQuizHandler = () => {
+    navigate('/quiz');
+  }
+  
   return (
     <div className='home'>
       {/* <h1 className="home-page-title">Select a number of choices and start the game</h1> */}
       <div className="question-number-container">
-        <span className="question-number ten">10</span>
-        <span className="question-number fifteen">15</span>
-        <span className="question-number twenty">20</span>
+        {numberOfQuestions && numberOfQuestions.map(option => (
+          <span key={option}>{option}</span>
+        ))}
       </div>
-      <button className="start-game-btn">Start Quiz</button>
+      <button 
+        className="start-game-btn"
+        onClick={startQuizHandler}>
+          Start Quiz
+      </button>
     </div>
   )
 }
--- src/redux/slices/quizSlice.js
+++ src/redux/slices/quizSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-
+  numberOfQuestions: [ 10, 13, 15 ]
 }
 
 const quizSlice = createSlice({
