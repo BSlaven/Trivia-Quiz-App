8a6227172693cfcfc4d5d44aacd4bac70be96dad
SlavenB <slaven.bunijevac@gmail.com>
2022-07-20 22:37:26 +0200

    add home route in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Home from './components/Home';
+// import Quiz from './components/Quiz';
 import './App.css'
 
 function App() {
 
   return (
-    <div className="app">
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>          
+          <Route path='/' element={<Home />} />
+          {/* <Route path='/quiz' element={<Quiz/>} /> */}
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
