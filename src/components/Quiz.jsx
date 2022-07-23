ca3e58ebc071d2378f95864d79953c7568f76dc1
SlavenB <slaven.bunijevac@gmail.com>
2022-07-23 12:44:53 +0200

    display answers without the correct one

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -9,7 +9,9 @@ const Quiz = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const [ questions, setQuestions ] = useState(data);
+  const [ questions, setQuestions ] = useState(data.results);
+  const [ currentIndex, setCurrentIndex ] = useState(1);
+  const [ currentQuestion, setCurrentQuestion ] = useState(questions[currentIndex])
 
   const routeBackHome = () => {
     navigate('/');
@@ -19,12 +21,11 @@ const Quiz = () => {
   return (
     <main className="main">
       <div className="question-container">
-        <h3 className="question-text">Ovo je prvo pitanje, pa tako još jednom?</h3>
+        <h3 className="question-text">{currentQuestion.question}</h3>
         <div className="answers-container">
-          <p className="answer">Odgovor 1</p>
-          <p className="answer correct">Odgovor 2</p>
-          <p className="answer wrong">Odgovor 3</p>
-          <p className="answer">Odgovor 4</p>
+          {currentQuestion.incorrect_answers && currentQuestion.incorrect_answers.map(answer => (
+            <span className="answer" key={answer}>{answer}</span>
+          ))}
         </div>
       </div>
       <button className="quit-btn" onClick={routeBackHome}>quit</button>
