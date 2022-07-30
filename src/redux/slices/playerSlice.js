2174377b79519ce683355e072ed32b6d164db9cf
SlavenB <slaven.bunijevac@gmail.com>
2022-07-30 12:34:12 +0200

    add reducers to increase correct and incorrect answers

--- src/redux/slices/playerSlice.js
+++ src/redux/slices/playerSlice.js
@@ -10,8 +10,15 @@ const playerSlice = createSlice({
   name: 'player',
   initialState,
   reducers: {
-    
+    increaseCorrectAnswers: (state, action) => {
+      state.correctAnswers = state.correctAnswers + 1
+    },
+    increaseIncorrectAnswers: (state, action) => {
+      state.incorrectAnswers = state.incorrectAnswers + 1
+    }
   }
-})
+});
+
+export const { increaseCorrectAnswers, increaseIncorrectAnswers } = quizSlice.actions;
 
 export default playerSlice.reducer;
