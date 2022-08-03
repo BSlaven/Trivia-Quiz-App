bee2f0d0d64d6295b8a5625429f04d57373e2af7
SlavenB <slaven.bunijevac@gmail.com>
2022-08-03 21:36:53 +0200

    setup structure in Stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { useNavigate } from "react-router"; 
+import { useSelector } from "react-redux/es/exports";
 
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <section className='stats-container'>
+      <div className='stats-control'>
+        <h3>Correct</h3>
+        <p>number of correct answers</p>
+      </div>
+      <div className='stats-control'>
+        <h3>Incorrect</h3>
+        <p>number of incorrect answers</p>
+      </div>
+      <div className='stats-control'>
+        <h3>Percentage</h3>
+        <p>correctnes percentage</p>
+      </div>
+      <button>Back Home</button>
+    </section>
   )
 }
 
