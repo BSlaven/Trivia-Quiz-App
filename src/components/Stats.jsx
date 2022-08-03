029e84169eb0fc364e80cd47ed533d1d87db841c
SlavenB <slaven.bunijevac@gmail.com>
2022-08-03 21:41:08 +0200

    enter values into Stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,19 +2,23 @@ import { useNavigate } from "react-router";
 import { useSelector } from "react-redux/es/exports";
 
 const Stats = () => {
+
+  const navigate = useNavigate();
+  const { correctAnswers, incorrectAnswers, percentage } = useSelector(store => store.player);
+  
   return (
     <section className='stats-container'>
       <div className='stats-control'>
         <h3>Correct</h3>
-        <p>number of correct answers</p>
+        <p>{correctAnswers}</p>
       </div>
       <div className='stats-control'>
         <h3>Incorrect</h3>
-        <p>number of incorrect answers</p>
+        <p>{incorrectAnswers}</p>
       </div>
       <div className='stats-control'>
         <h3>Percentage</h3>
-        <p>correctnes percentage</p>
+        <p>{percentage}</p>
       </div>
       <button>Back Home</button>
     </section>
